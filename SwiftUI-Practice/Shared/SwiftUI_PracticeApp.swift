bb94a0aa50f9d358eb8f8940a51b0d81eb25d08d
BBHHancock <bai.szsy@qq.com>
2021-11-24 16:00:24 +0800

    init - add a multiple platform project

---
+++ SwiftUI-Practice/Shared/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  Shared
+//
+//  Created by hancock on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
